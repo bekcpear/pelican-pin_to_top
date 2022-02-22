baed14055f716cb74b6e8edb90b8a390bbd7c6e3
Ryan Qian <i@bitbili.net>
2022-02-22 23:01:36 +0800

    change to: pin articles by specified slugs in the setting 'PIN_TO_TOP' array,
    with the specified order
    
    Signed-off-by: Ryan Qian <i@bitbili.net>

--- pin_to_top.py
+++ pin_to_top.py
@@ -9,14 +9,26 @@ from pelican import signals
 
 def update_pinned_articles(generator):
     new_order = []
-    # count articles and keep the pinned ordered by date
-    pinned = 0;
+
+    # pin articles by slug in the setting PIN_TO_TOP with the list's order
+    pin_articles = {}
+    pin_articles_slugs = generator.settings.get('PIN_TO_TOP')
+
     for article in generator.articles:
-        if hasattr(article,'pin'):
-            new_order.insert(pinned,article)
-            pinned += 1
-        else:
+        pin_article = False
+        for slug in pin_articles_slugs:
+            if slug == article.slug:
+                pin_articles[slug] = article
+                pin_article = True
+                break
+        if not pin_article:
             new_order.append(article)
+
+    pined = 0
+    for slug in pin_articles_slugs:
+        new_order.insert(pined, pin_articles[slug])
+        pined += 1
+
     generator.articles = new_order
     # Update the context with the new list
     generator.context['articles'] = generator.articles
